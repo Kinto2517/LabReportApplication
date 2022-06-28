d56f9f3cc45198c94035318bf83e50509a680545
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-06-28 06:51:52 +0300

    Adding Get Mapping for listng reports.

--- src/main/java/com/example/labreport/controller/ReportController.java
+++ src/main/java/com/example/labreport/controller/ReportController.java
@@ -1,30 +1,46 @@
 package com.example.labreport.controller;
 
+import com.example.labreport.business.services.ReportService;
+import com.example.labreport.business.services.UserService;
+import com.example.labreport.model.Report;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/reports")
 public class ReportController {
 
+    private ReportService reportService;
+    private UserService userService;
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
 
-    @GetMapping("/users")
-    public String users() {
-        return "users";
-    }
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @Autowired
+    public ReportController(ReportService reportService, UserService userService) {
+        this.reportService = reportService;
+        this.userService = userService;
     }
 
-    @GetMapping("/unauthorized")
-    public String unauthorized() {
-        return "unauthorized";
+
+
+    @GetMapping("/reports")
+    public String reports(Model model) {
+
+        List<Report> reports = reportService.findAll();
+
+        model.addAttribute("reports", reports);
+
+        return "reports";
     }
+
+
+
+
+
 
 
 
