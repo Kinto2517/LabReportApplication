42925db3b905980a0ad15796b6bfa82653134ac3
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-06-28 03:52:48 +0300

    Adding Get Mapping for login and home page.

--- src/main/java/com/example/labreport/controller/UserController.java
+++ src/main/java/com/example/labreport/controller/UserController.java
@@ -1,4 +1,27 @@
 package com.example.labreport.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class UserController {
+
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/users")
+    public String users() {
+        return "users";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/unauthorized")
+    public String unauthorized() {
+        return "unauthorized";
+    }
+
 }
