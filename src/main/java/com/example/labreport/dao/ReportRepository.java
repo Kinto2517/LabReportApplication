1bb3800095685a231dfd194d588bb361a0f074f2
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-06-29 09:55:04 +0300

    Added Repository annotation.

--- src/main/java/com/example/labreport/dao/ReportRepository.java
+++ src/main/java/com/example/labreport/dao/ReportRepository.java
@@ -4,9 +4,11 @@ import com.example.labreport.model.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
 
