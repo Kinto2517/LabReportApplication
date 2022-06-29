dad216757582ce51e0ab58f92b379adecc8073c4
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-06-29 05:24:06 +0300

    Added download, delete and update Mappings. Added order and addReport Mapping.

--- src/main/java/com/example/labreport/controller/ReportController.java
+++ src/main/java/com/example/labreport/controller/ReportController.java
@@ -3,13 +3,17 @@ package com.example.labreport.controller;
 import com.example.labreport.business.services.ReportService;
 import com.example.labreport.business.services.UserService;
 import com.example.labreport.model.Report;
+import com.example.labreport.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -28,17 +32,30 @@ public class ReportController {
 
 
 
+
+        @GetMapping("/download")
+        public ResponseEntity<?> download(@RequestParam("id") Long id)
+        {
+            Report report = reportService.findById(id);
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION,
+                            "attachment; filename=\"" + report.getPatient_name() + "\"." + report.getExtension())
+                    .body(new ByteArrayResource(report.getReport_img()));
+        }
+
+
+
     @GetMapping("/search")
-    public String delete(@RequestParam("reportParam") String search, Model theModel)
+    public String search(@RequestParam("reportParam") String search, Model model)
     {
         List<Report> reports = reportService.searchBy(search);
 
-        theModel.addAttribute("reports", reports);
+        model.addAttribute("reports", reports);
 
         return "reports";
     }
 
-    @GetMapping("/")
+    @GetMapping("/main")
     public String reports(Model model) {
 
         List<Report> reports = reportService.findAll();
@@ -48,11 +65,80 @@ public class ReportController {
         return "reports";
     }
 
+    @GetMapping("/delete")
+    public String deleteUser(@RequestParam Long id ) {
+
+        Report report = reportService.findById(id);
+
+        if (report != null) {
+            reportService.delete(id);
+        }
+
+        return "redirect:/reports/main";
+
+    }
+
+
+    @GetMapping("/addForm")
+    public String addReport(Model model) {
+
+        Report report = new Report();
+        List<User> users = userService.getAllUsers();
+
+
+        model.addAttribute("users",users);
+        model.addAttribute("report", report);
+        return "addReportForm";
+
+    }
+
+
+    @GetMapping("/update")
+    public String update(@RequestParam("id") Long id, Model model) {
+
+        Report report = reportService.findById(id);
+        List<User> users = userService.getAllUsers();
+
+        model.addAttribute("users",users);
+        model.addAttribute("report", report);
+
+        return "updateReportForm";
+    }
 
 
 
+    @PostMapping("/addReport")
+    public String addReport(@Valid @ModelAttribute("report") Report report, BindingResult result) {
+        if (result.hasErrors()) {
+            return "addReportForm";
+        }
+        reportService.save(report);
+
+        return "redirect:/reports/main";
+    }
 
 
+    boolean orderAsc = true;
+    @GetMapping("/order")
+    public String order(Model model)
+    {
+
+        List<Report> reports;
+
+        if(orderAsc)
+        {
+            reports = reportService.orderByAsc();
+            orderAsc = false;
+        }
+        else
+        {
+            reports = reportService.orderByDesc();
+            orderAsc = true;
+        }
+
+        model.addAttribute("reports", reports);
+        return "reports";
+    }
 
 
 }
